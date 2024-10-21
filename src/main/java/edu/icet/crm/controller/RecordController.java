2671ece0651f0869ac5d44ee45f899845eda75e3
yuwantha98 <yuwantharavihara123@gmail.com>
2024-10-21 09:46:42 +0530

    Added delete record

--- src/main/java/edu/icet/crm/controller/RecordController.java
+++ src/main/java/edu/icet/crm/controller/RecordController.java
@@ -1,4 +1,18 @@
 package edu.icet.crm.controller;
 
+import edu.icet.crm.service.RecordService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/record")
 public class RecordController {
+    @Autowired
+    RecordService service;
+
+    @DeleteMapping("/{id}")
+    public void deleteRecord(@PathVariable String id){
+        service.deleteRecord(id);
+    }
 }
--- src/main/java/edu/icet/crm/service/impl/RecordServiceImpl.java
+++ src/main/java/edu/icet/crm/service/impl/RecordServiceImpl.java
@@ -1,4 +1,18 @@
 package edu.icet.crm.service.impl;
 
-public class RecordServiceImpl {
+import edu.icet.crm.repository.RecordRepository;
+import edu.icet.crm.service.RecordService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RecordServiceImpl implements RecordService {
+
+    @Autowired
+    RecordRepository repository;
+
+    @Override
+    public void deleteRecord(String id) {
+        repository.deleteById(id);
+    }
 }
