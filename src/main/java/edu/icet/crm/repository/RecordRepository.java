668ef71394e76190c3306fdffe80aaf42ec43813
nethmaKannangara <nethmakannangara007@gmail.com>
2024-10-21 14:41:02 +0530

    implement the table relationship between RecordEntity and ReportEntity

--- src/main/java/edu/icet/crm/repository/RecordRepository.java
+++ src/main/java/edu/icet/crm/repository/RecordRepository.java
@@ -1,6 +1,7 @@
 package edu.icet.crm.repository;
 
 import edu.icet.crm.entity.RecordEntity;
+import edu.icet.crm.entity.ReportEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RecordRepository extends JpaRepository<RecordEntity,String> {
--- src/main/java/edu/icet/crm/service/impl/RecordServiceImpl.java
+++ src/main/java/edu/icet/crm/service/impl/RecordServiceImpl.java
@@ -3,8 +3,10 @@ package edu.icet.crm.service.impl;
 import edu.icet.crm.repository.RecordRepository;
 import edu.icet.crm.service.RecordService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
+@Service
 public class RecordServiceImpl implements RecordService {
 
     final RecordRepository recordRepository;
