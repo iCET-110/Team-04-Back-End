91081a87cd641ac6018fde5e791ba31f698c0cc1
nethmaKannangara <nethmakannangara007@gmail.com>
2024-10-21 12:50:39 +0530

    Replacing dev branch 1

--- src/main/java/edu/icet/crm/entity/RecordEntity.java
+++ src/main/java/edu/icet/crm/entity/RecordEntity.java
@@ -1,4 +1,21 @@
 package edu.icet.crm.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.util.Date;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class RecordEntity {
+    private String recordId;
+    private String patientID;
+    private Date recordDate;
+    private String description;
+    private List<ReportEntity> reportList;
 }
--- src/main/java/edu/icet/crm/entity/ReportEntity.java
+++ src/main/java/edu/icet/crm/entity/ReportEntity.java
@@ -1,4 +1,19 @@
 package edu.icet.crm.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class ReportEntity {
+    private String reportId;
+    private String reportLink;
+    private String categoryType;
+    private List<RecordEntity> recordList;
 }
