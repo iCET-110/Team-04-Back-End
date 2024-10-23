a418993e2ab0e96b37d9b088ca233e6d3bc71037
yuwantha98 <yuwantharavihara123@gmail.com>
2024-10-23 11:24:03 +0530

    add the try-catch

--- src/main/java/edu/icet/crm/controller/RecordController.java
+++ src/main/java/edu/icet/crm/controller/RecordController.java
@@ -2,20 +2,25 @@ package edu.icet.crm.controller;
 
 import edu.icet.crm.service.impl.RecordServiceImpl;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
 @CrossOrigin
+@RequestMapping("/record")
 public class RecordController {
 
     final RecordServiceImpl recordService;
 
     @DeleteMapping("/{id}")
-    public void deleteRecord(@PathVariable String id){
-        recordService.deleteRecord(id);
+    public ResponseEntity<String> deleteRecord(@PathVariable String id) {
+        try {
+            recordService.deleteRecord(id);
+            return new ResponseEntity<>("Record deleted successfully", HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>("Record not found", HttpStatus.NOT_FOUND);
+        }
     }
 }
